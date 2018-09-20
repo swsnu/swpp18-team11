9449f2e534e52c1394f9f3164b2c5edc18c9ff4f
Algy <algy@dunamu.com>
2018-09-20 17:42:10 +0900

    Added Order TTS

--- backend/kiwi/kiorder/urls.py
+++ backend/kiwi/kiorder/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import hello_world
+from .views import hello_world, order_tts
 
 urlpatterns = [
-    path('hello', hello_world)
+    path('hello', hello_world),
+    path('api/v1/order_tts', order_tts),
 ]
--- backend/kiwi/kiorder/views.py
+++ backend/kiwi/kiorder/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
+from gtts import gTTS
+from io import BytesIO
+from base64 import b64encode
 
 
 # Create your views here.
@@ -7,4 +10,18 @@ from django.http import HttpResponse
 
 def hello_world(request):
     return HttpResponse("HELLO!")
+
+
+def order_tts(request):
+    try:
+        number = int(request.GET['number'])
+        text = f"{number}번 손님 음식 나왔습니다"
+        gtts = gTTS(text, lang="ko")
+    except (TypeError, ValueError):
+        return HttpResponseBadRequest()
+    io = BytesIO()
+    gtts.write_to_fp(io)
+    b64ed_mp3 = b64encode(io.getvalue()).decode()
+    url = f"data:audio/mpeg;base64,{b64ed_mp3}"
+    return JsonResponse({"url": url})
 
