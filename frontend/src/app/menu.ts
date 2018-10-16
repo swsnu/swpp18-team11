c49fbcbe6d976444c1581a68f32041ce7a77ac81
hintyu <19sy1107!>
2018-10-16 12:01:32 +0900

    Initial setup

---
+++ frontend/src/app/menu.ts
@@ -0,0 +1,10 @@
+import { Option } from "./option";
+
+export class Menu {
+
+  id: number
+  name: string
+  thumbnail: string
+  base_price: number
+  options: Option[]
+}
---
+++ frontend/src/app/option.ts
@@ -0,0 +1,7 @@
+
+export class Option {
+  id: number
+  name: string
+  base_price: number
+  max_capacity: number
+}
