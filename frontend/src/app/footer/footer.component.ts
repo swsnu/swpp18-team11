574de9b4a84871c7ec1d7548735041b2f4cbe0b3
hintyu <19sy1107!>
2018-11-10 14:32:45 +0900

    Added header, footer components

---
+++ frontend/src/app/footer/footer.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { MyCartService } from "../my-cart.service";
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  myCartCount: number
+  constructor(
+    private myCartService: MyCartService
+  ) { }
+
+  ngOnInit() {
+    this.setMyCartCount()
+  }
+
+  emptyMyCart(): void{
+    if(confirm('장바구니를 비우시겠습니까?')){
+      this.myCartService.emptyMyCart()
+      this.setMyCartCount()
+    }
+  }
+  // TODO: Make async version of myCartService!
+  setMyCartCount():void {
+    this.myCartCount = this.myCartService.getMyCartCount()
+  }
+}
