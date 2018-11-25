e93bd88ff528020cfadb18a966ad7070738174f2
Alchan Kim <a9413miky@gmail.com>
2018-11-25 15:23:20 +0900

    Draft deployment

--- backend/kiwi/kiwi/settings.py
+++ backend/kiwi/kiwi/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 AUTH_USER_MODEL = "kiorder.User"
