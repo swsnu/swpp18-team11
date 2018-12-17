a71989e99765dda6f396dd1fdf7998c1730f7ec2
kyunggeun-lee <initium0917@snu.ac.kr>
2018-12-17 23:34:40 +0900

    lint

--- frontend/src/app/user.service.ts
+++ frontend/src/app/user.service.ts
@@ -1,4 +1,4 @@
-import {EventEmitter, Injectable} from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
